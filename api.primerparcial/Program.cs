de25bd6864cc2102ffb8db839c967fd1654b8da7
devpbeat <devpbrn@outlook.com>
2022-10-19 19:17:21 -0300

    changes

--- api.primerparcial/Program.cs
+++ api.primerparcial/Program.cs
@@ -8,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
 
 builder.Services.AddDbContext<PrimerParcialApp>(options =>
